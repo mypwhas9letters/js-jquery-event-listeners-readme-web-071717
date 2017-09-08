cf4aa84ba6728ab57648528ce4fdfbbbf38f3e9b
Hui Wang <huiw3939@gmail.com>
2017-09-08 16:49:29 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
+function getIt(){
+  $("p").on("click", alert("HEY!"))
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $(this).addClass("tasty")
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", alert("Your form is going to be submitted now."))
+}
+
+function pressIt(){
+    $("#typing").on("keydown", function(e){
+      if(e.which === 71){
+        alert("You pressed 'g'!")
+      }
+    })
+}
 
 $(document).ready(function(){
-
 // call functions here
-
 });
